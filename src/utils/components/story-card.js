a3262669812b6acdd1a5e68264abbe9a3ce068fa
Joshua Abu <joshujah007@gmail.com>
2017-03-22 19:29:26 -0400

    styled the story cards

--- src/utils/components/story-card.js
+++ src/utils/components/story-card.js
@@ -3,7 +3,8 @@ import React, { PropTypes } from 'react'
 export const StoryCard = ({
   avatar,
   author,
-  dateTime
+  dateTime,
+  story
 }) => {
   return (
     <div className="card-panel white card-story">
@@ -13,20 +14,36 @@ export const StoryCard = ({
         <div className="user_metadata">
           <a className="link link-accent">{author || `Joshua Abu`}</a>
           <div className="story-date">
-            <time dateTime={dateTime || "2017-03-20T01:16:58.224Z"}>
+            <time dateTime={dateTime || "2017-03-13T01:16:58.224Z"}>
               {`March 13`}
             </time></div>
         </div>
       </div>
-      <div className="story-title">
-
+      <div className="story-content">
+        <a>
+          <h3 className="story-title">I have a story for you.</h3>
+          <h4 className="story-sub-title">True Story.</h4>
+        </a>
+      </div>
+      <div className="story-snippet">
+        <span>{story || `I am a very simple card. I am good at containing small bits of information.
+        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.`}
+        </span>
       </div>
 
+      <div className="article-readMore">
+        <a>
+          <p>Read More..</p>
+        </a>
+      </div>
 
-
-      <span>I am a very simple card. I am good at containing small bits of information.
-      I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-      </span>
     </div>
   )
+}
+
+StoryCard.propTypes = {
+  avatar: PropTypes.string,
+  author: PropTypes.string,
+  dateTime: PropTypes.string,
+  story: PropTypes.string
 }
